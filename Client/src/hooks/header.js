413c6a75e3355479f7927de6536f548091ebb1a1
FranklinMiranda <fdm61344@gmail.com>
2023-03-08 14:21:55 -0500

    CM

--- Client/src/hooks/header.js
+++ Client/src/hooks/header.js
@@ -11,26 +11,22 @@ const Header = () => {
         Home
       </Link>
       <Link to="/profile" style={{ padding: '5px' }}>
-        {' '}
         Profile
       </Link>
       <Link to="/hooksform" style={{ padding: '5px' }}>
-        {' '}
         Hooks Form
       </Link>
       <Link to="/hookscontainer" style={{ padding: '5px' }}>
-        {' '}
         Hooks Container
       </Link>
       <Link to="/privateroute" style={{ padding: '5px' }}>
-        {' '}
         Private Route
       </Link>
-      {!context.authState ? (
+      {!context.authState ? 
         <button onClick={() => context.authObj.login()}>Login</button>
-      ) : (
+       : 
         <button onClick={() => context.authObj.logout()}>Logout</button>
-      )}
+      }
     </div>
   );
 };
